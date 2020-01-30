83b7b28b059051ef7622ff8339e53e763a1335e8
β︎Δℕ︎Σ β︎ℐ︎ⅅ︎ⅅ︎ℐ︎✕︎ ⛧ <1337god@cock.email>
2020-01-30 10:46:04 +0000

    Update black.cr

--- black.cr
+++ black.cr
@@ -1,1 +1,10 @@
-puts "The people of this world believe in Garnet, Amethyst and Pearl, and Steven!"
+# A very basic HTTP server
+require "http/server"
+
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello world, got #{context.request.path}!"
+end
+
+puts "Listening on http://127.0.0.1:8080"
+server.listen(8080)
