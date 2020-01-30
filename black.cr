f690c7eddc6a0cfbf3d6942ce3a0aefd3f1e3ae3
β︎Δℕ︎Σ β︎ℐ︎ⅅ︎ⅅ︎ℐ︎✕︎ ⛧ <1337god@cock.email>
2020-01-30 10:49:13 +0000

    Update black.cr

--- black.cr
+++ black.cr
@@ -3,7 +3,7 @@ require "http/server"
 
 server = HTTP::Server.new do |context|
   context.response.content_type = "text/plain"
-  context.response.print "Hello world, got #{context.request.path}!"
+  context.response.print "Hello world, Programmed to work and not to feel Not even sure that this is real, got #{context.request.path}!"
 end
 
 puts "Listening on http://127.0.0.1:8080"
